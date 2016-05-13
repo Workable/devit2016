89f926ece7281786121d0ec24f98f43ca547e07b
Angelos Karagkiozidis <angelkar@gmail.com>
2016-05-13 15:55:24 +0300

    Add fake delay

--- server/src/server.js
+++ server/src/server.js
@@ -16,6 +16,14 @@ const middlewares = jsonServer.defaults();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
+// Add a fake delay in responses, we want to show some loaders
+server.use(function (req, res, next) {
+  var delay = Math.round(Math.random() * 3000);
+  setTimeout(function() {
+    next()
+  }, delay);
+})
+
 server.use(router);
 server.listen(1337, function () {
   console.log('Winter is coming ❄️')
