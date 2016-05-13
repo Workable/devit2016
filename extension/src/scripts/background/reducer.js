96f7ff3fac489b3d0241ec434d4f5df05b98ea94
Sokratis Vidros <sokratis.vidros@gmail.com>
2016-05-14 01:20:48 +0300

    Enforce immutability on background store.

--- extension/src/scripts/background/reducer.js
+++ extension/src/scripts/background/reducer.js
@@ -3,9 +3,9 @@ import {SELECT_NAME, RECEIVE_SPOILER} from 'shared/actionTypes';
 export default (state = {}, action) => {
   switch (action.type) {
     case SELECT_NAME:
-      return Object.assign(state, { name: action.name });
+      return Object.assign({}, state, { name: action.name });
     case RECEIVE_SPOILER:
-      return Object.assign(state, { spoiler: action.spoiler });
+      return Object.assign({}, state, { spoiler: action.spoiler });
     default:
       return state;
   };
